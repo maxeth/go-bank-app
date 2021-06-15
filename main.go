eb0aa9b42547b3b89eb215b4ab4a21d876477481
Maximilian Müller <tiecy02@gmail.com>
2021-06-15 18:40:58 +0200

    api and db setup

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	_ "github.com/lib/pq"
+	"github.com/maxeth/go-bank-app/api"
+	db "github.com/maxeth/go-bank-app/db/sqlc"
+)
+
+const (
+	address = "0.0.0.0:8080"
+)
+
 func main() {
+	conn, err := db.GetOrCreate()
+	if err != nil {
+		panic("cannot connect to db")
+	}
+
+	repo := db.NewRepository(conn.DB)
+	server := api.NewServer(repo)
+
+	err = server.Start(address)
+	if err != nil {
+		panic("server couldn't start")
+	}
 
 }
