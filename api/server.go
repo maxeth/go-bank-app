54e9e5e727f86aa31c87ea4ac6ea6a11c91d21ee
Maximilian Müller <tiecy02@gmail.com>
2021-06-17 21:08:14 +0200

    added custom currency validation function

--- api/server.go
+++ api/server.go
@@ -2,6 +2,9 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+
 	db "github.com/maxeth/go-bank-app/db/sqlc"
 )
 
@@ -14,9 +17,15 @@ func NewServer(repo db.Repository) *Server {
 	server := &Server{repository: repo}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
