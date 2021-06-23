562211fb4e64caae51121dff7d98853f4eb22ba4
Maximilian Müller <tiecy02@gmail.com>
2021-06-23 17:46:12 +0200

    added config to server setup

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"math/rand"
+	"time"
+
 	_ "github.com/lib/pq"
 	"github.com/maxeth/go-bank-app/api"
+	"github.com/maxeth/go-bank-app/config"
 	db "github.com/maxeth/go-bank-app/db/sqlc"
 )
 
@@ -11,17 +15,29 @@ const (
 )
 
 func main() {
-	conn, err := db.GetOrCreate()
+	conf, err := config.New(".")
+	if err != nil {
+		panic("cannot connect to db")
+	}
+
+	conn, err := db.GetOrCreate(conf)
 	if err != nil {
 		panic("cannot connect to db")
 	}
 
 	repo := db.NewRepository(conn.DB)
-	server := api.NewServer(repo)
+	server, err := api.NewServer(conf, repo)
+	if err != nil {
+		panic("couldnt create new instance of a server")
+	}
 
 	err = server.Start(address)
 	if err != nil {
 		panic("server couldn't start")
 	}
 
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
