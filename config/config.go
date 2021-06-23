e01c0d8599d9b3dae10c92590641c92e7ace6f57
Maximilian Müller <tiecy02@gmail.com>
2021-06-23 17:51:24 +0200

    new token auth variables

--- config/config.go
+++ config/config.go
@@ -1,6 +1,11 @@
 package config
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
 	"github.com/spf13/viper"
 )
 
@@ -10,13 +15,19 @@ const (
 )
 
 type Config struct {
-	Environment   string
-	DBString      string `mapstructure:"DB_STRING"`
-	DBDriver      string `mapstructure:"DB_DRIVER"`
-	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	Environment         string
+	DBString            string        `mapstructure:"DB_STRING"`
+	DBDriver            string        `mapstructure:"DB_DRIVER"`
+	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSummetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 }
 
 func New(path string) (config Config, err error) {
+
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	fmt.Println(dir)
+
 	viper.AddConfigPath(path) // this refers to the path of the file/directory that calls this function, not the path of this config.go function
 	viper.SetConfigName("app")
 	viper.SetConfigType("env")
